bf033aa012fd0c7cf3739e8f1969b6f5be1ef209
fmway <fm18lv@gmail.com>
2025-07-12 18:31:14 +0700

    chore(pkgs/cloudflare-warp): typos

--- pkgs/cloudflare-warp.nix
+++ pkgs/cloudflare-warp.nix
@@ -2,7 +2,6 @@
   inherit (pkg) pname name version meta;
   paths = [pkg];
   postBuild = /* sh */ ''
-    mkdir $out
     cp -rf * $out/
     rm -rf $out/etc $out/lib
   ''; # remove autostart, very annoying
