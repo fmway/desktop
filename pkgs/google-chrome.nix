8fc0cb1a619c4adcb50df4d00bbeefb3393bb345
fmway <fm18lv@gmail.com>
2025-08-03 15:11:24 +0700

    chore(pkgs/google-chrome): add some flags to enable vulkan

--- pkgs/google-chrome.nix
+++ pkgs/google-chrome.nix
@@ -1,6 +1,6 @@
 {
   __input.commandLineArgs.__append = [
-    "--enable-features=VaapiVideoDecodeLinuxGL,TouchpadOverscrollHistoryNavigation"
+    "--enable-features=VaapiVideoDecodeLinuxGL,TouchpadOverscrollHistoryNavigation,AcceleratedVideoEncoder,VaapiVideoEncoder,Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport,UseMultiPlaneFormatForHardwareVideo"
     "--use-gl=angle"
     "--use-angle=gl"
     "--ozone-platform=wayland"
