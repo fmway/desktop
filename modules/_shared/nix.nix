831763a7d21262870d732b2fbf6958d8114c4aea
fmway <fm18lv@gmail.com>
2025-08-23 07:09:37 +0700

    chore(modules/nix): `nix.gc.frequency` to `nix.gc.dates` (home-manager)

--- modules/_shared/nix.nix
+++ modules/_shared/nix.nix
@@ -9,31 +9,22 @@
     auto-optimise-store = lib.mkDefault false;
   };
 
-  nix.gc = {
-    automatic = true;
-    options = "--delete-older-than 3d";
-  };
-   
-  # automatic nix gc every mondays and fridays
-  imports = let
-    dates = "Mon,Fri *-*-* 00:00:00";
-  in lib.optionals (name == "nixosModules")
-    [ { nix.gc.dates = dates; } ]
-  ++ lib.optionals (name == "homeManagerModules")
-    [ { nix.gc.frequency = dates; } ]
-  ++ lib.optionals (name == "nixDarwinModules")
-    [
-      { nix.gc.interval = [
-        {
-          Hour = 0;
-          Minute = 0;
-          WeekDay = 5;
-        }
-        {
-          Hour = 0;
-          Minute = 0;
-          WeekDay = 1;
-        }
-      ]; }
-    ];
+  nix.gc = lib.mkMerge [
+    {
+      automatic = true;
+      options = "--delete-older-than 3d";
+    }
+
+    # automatic nix gc every mondays and fridays
+    # can't use lazy mkIf because interval doesn't exist in nixos/home-manager, and vice versa
+    (let
+      key  = if name == "nixDarwinModules" then ["interval"] else ["dates"];
+      value= if name == "nixDarwinModules" then
+        [
+          { Hour = 0; Minute = 0; WeekDay = 5; }
+          { Hour = 0; Minute = 0; WeekDay = 1; }
+        ]
+      else "Mon,Fri *-*-* 00:00:00";
+    in lib.setAttrByPath key value)
+  ];
 }
