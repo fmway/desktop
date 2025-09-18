7da64308b2cde45421390bce8357d2a7f56d8d65
fmway <fm18lv@gmail.com>
2025-09-18 23:01:38 +0700

    chore(direnv): add flox direnv hook

--- modules/_shared/direnv/default.nix
+++ modules/_shared/direnv/default.nix
@@ -1,6 +1,13 @@
 { internal, _file, name, inputs, selfInputs ? inputs, ... }:
 { inputs, pkgs, config, lib, ... }: let
-  direnvrc = builtins.readFile ./direnv.sh;
+  flox_direnv = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/flox/flox-direnv/v1.1.0/direnv.rc";
+    hash = "sha256-c2YCane8WGmYeCDc9wIZyVL8AgbdfhPaEoM+5aFuysw=";
+  };
+  direnvrc = lib.concatStringsSep "\n" [
+    (builtins.readFile ./direnv.sh)
+    (builtins.readFile flox_direnv)
+  ];
 in {
   inherit _file;
   programs.direnv = lib.mkMerge [
