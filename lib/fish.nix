0c8695b2ff5a2f230b131f7509f6c60de1d7cbeb
fmway <fm18lv@gmail.com>
2025-10-07 18:17:14 +0700

    chore(lib/fish): add bind

--- lib/fish.nix
+++ lib/fish.nix
@@ -1,5 +1,27 @@
-{ lib, ... }:
-{
+{ lib, ... }: let
+  mkBind = initial: keys: x:
+  if builtins.isAttrs x then
+    { inherit keys; } // initial // x
+  else mkBind (initial // {
+    commands = initial.commands or [] ++ lib.fmway.flat x;
+  }) keys;
+  op = { bind' = "preset"; bind_ = "user"; bind = null; };
+  modes = [ "default" "insert" "replace" "replace_one" "visual" ];
+  setsBind = lib.listToAttrs (lib.mapAttrsToList (name: operate: {
+    inherit name;
+    value = let
+      i = lib.optionalAttrs (!isNull operate) { inherit operate; };
+      mkErase = initial: mkBind ({ erase = true; } // initial);
+      r = lib.listToAttrs (map (x: {
+        name = if isNull x then "__functor" else x;
+        value = let
+          i' = i // lib.optionalAttrs (!isNull x) { mode = x; };
+          y = mkBind i';
+        in if isNull x then _: y else { __functor = _: y; erase = mkErase i'; };
+      }) ([ null ] ++ modes));
+    in { erase = mkErase i; } // r;
+  }) op);
+in setsBind // {
   /*
     fish.importFunctions :: Path :: Attrs
     auto imports all file with .fish ext, use this in programs.fish.functions
