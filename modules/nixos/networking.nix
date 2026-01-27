193a0275791d915b51433c60433c99b3e1e91738
fmway <fm18lv@gmail.com>
2026-01-27 20:34:15 +0700

    chore: use quad9 instead of cloudflare dns

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -13,7 +13,10 @@
     networkmanager.dns = lib.mkDefault "systemd-resolved";
 
     nameservers = [
-      "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"
+      # "1.1.1.1#one.one.one.one"
+      # "1.0.0.1#one.one.one.one"
+      "9.9.9.9#dns.quad9.net"
+      "149.112.112.112#dns.quad9.net"
     ];
 
     # Proxy
