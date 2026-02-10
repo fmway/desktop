784491cc2e35a763afa5188319c3c9b51b998ceb
fmway <fm18lv@gmail.com>
2026-02-10 23:29:29 +0700

    chore(secrets): rekey system

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   keys = {
-    system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO58iqlTtlgkt9e8u7X6hbBusv6yf9LGJGk16/YhVDOw";
+    system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOcO6Gzc82fRDIb+DtYMM9yQzz6N1w31aQcj2tQUaRZj";
     fmway = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGM2l3zhUoxYpBzzmH7KsWdo1XMrc1eCgNrwaIHVM2pZ";
   };
   
