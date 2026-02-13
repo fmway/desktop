87de6c22406f9a62472c5855871cbb446d46b8c0
fmway <fm18lv@gmail.com>
2026-02-13 17:03:53 +0700

    chore(firefox): ...

--- home/fmway/firefox-extension.nix
+++ home/fmway/firefox-extension.nix
@@ -1,18 +1,19 @@
 pkgs: (with pkgs.nur.repos.rycee.firefox-addons; [
   metamask
-  multi-account-containers
-  greasemonkey
-  gesturefy
-  tree-style-tab
-  react-devtools
+  # multi-account-containers
+  violentmonkey
+  # greasemonkey
+  # gesturefy
+  # tree-style-tab
+  # react-devtools
   # search-by-image
-  firefox-color
-  vue-js-devtools
+  # firefox-color
+  # vue-js-devtools
 ]) ++ pkgs.lib.optionals (pkgs ? fmway) (with pkgs.fmway.firefox-addons; [
   xdm_v8
   what-font
-  wakatime
-  stayfree
+  # wakatime
+  # stayfree
   firefox-relay
-  preact-devtools
+  # preact-devtools
 ])
