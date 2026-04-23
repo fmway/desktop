738802b4e7dfca97449da64ce94b5ec704833f3e
fmway <fm18lv@gmail.com>
2026-04-23 22:20:45 +0700

    Initial flakes

---
+++ flake.nix
@@ -0,0 +1,20 @@
+# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# Use `nix run .#write-flake` to regenerate it.
+{
+  outputs = inputs: import ./outputs.nix inputs;
+
+  inputs = {
+    den.url = "github:vic/den/v0.16.0";
+    flake-file.url = "github:vic/flake-file";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    fmway-lib = {
+      url = "github:fmway/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    import-tree.url = "github:vic/import-tree";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+  };
+}
---
+++ modules/default.nix
@@ -0,0 +1,26 @@
+{ inputs, ... }:
+{
+
+  imports = [
+    inputs.flake-file.flakeModules.default
+  ];
+
+  systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+
+  flake-file.inputs = {
+    # core flake
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    fmway-lib = {
+      url = "github:fmway/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    den.url = "github:vic/den/v0.16.0";
+    flake-file.url = "github:vic/flake-file";
+    import-tree.url = "github:vic/import-tree";
+  };
+
+}
