9bf3795bd2c446044d887ceb73c7c1df35f89fb7
fmway <fm18lv@gmail.com>
2026-05-11 13:38:56 +0700

    Add keyd group

--- modules/services/keyd/default.nix
+++ modules/services/keyd/default.nix
@@ -1,7 +1,22 @@
 # TODO: expose as schema / keyd class
 { lib, ... }:
 {
-  fmx.services._.keyd.nixos = { pkgs, ... }: {
+  fmx.services._.keyd.includes = [
+    # register keyd group for each user
+    ({ user, host, ... }: {
+      nixos.users.users.${user.userName}.extraGroups = [ "keyd" ];
+    })
+    { nixos.users.groups.keyd = {}; }
+  ];
+  fmx.services._.keyd.nixos = { pkgs, ... }:
+  {
+    # Link keyd-keyboard to /dev/input/keyd
+    services.udev.extraRules = /* udev */ ''
+      SUBSYSTEM=="input", \
+        ATTRS{name}=="keyd virtual keyboard", \
+        SYMLINK+="input/keyd"
+    '';
+
     environment.etc."libinput/local-overrides.quirks".text = /* ini */ ''
       [Serial Keyboards]
       MatchUdevType=keyboard
@@ -25,69 +40,7 @@
       enable = true;
       keyboards.default = {
         ids = [ "*" ];
-        settings = lib.keyd.parse (self: with self; {
-          # default / insert mode
-          main = {
-            henkan           = layer control;
-            muhenkan         = layer alt;
-            katakanahiragana = layer altgr;
-            capslock         = layer normal;
-            shift            = layer shift;
-          };
-          shift.capslock = capslock;
-          # normal mode
-          normal = {
-            h   = left;
-            j   = down;
-            k   = up;
-            l   = right;
-            b   = oneshot normal;
-            i   = clear; # back to insert mode
-            "[" = toggle normal;
-            v   = toggle visual;
-            g   = oneshot g_layer;
-
-            shift."6" = home;
-            shift."4" = end;
-            shift."h" = pageup;
-            shift."j" = pagedown;
-            shift."g" = Ctrl end;
-
-            f1 = mute;
-            f2 = volumedown;
-            f3 = volumeup;
-            f4 = micmute;
-            f5 = brightnessdown;
-            f6 = brightnessup;
-            f7 = display; # maybe
-            f8 = connect; # maybe
-            # f9 = ...;
-            f10 = bluetooth;
-            # f11 = ...;
-            f12 = bookmarks;
-          };
-
-          g_layer = rec {
-            g          = Ctrl home;
-            normal.g   = g;
-          };
-
-          visual = {
-            h   = Shift left;
-            j   = Shift down;
-            k   = Shift up;
-            l   = Shift right;
-
-            shift."6" = Shift home;
-            shift."4" = Shift end;
-            shift."h" = Shift pageup;
-            shift."j" = Shift pagedown;
-
-            y   = clearm (Ctrl v);
-            x   = clearm (Ctrl x);
-            esc = clearm esc;
-          };
-        });
+        settings = lib.keyd.parse (import ./_keymap.nix);
       };
     };
   };
