68abc6b4ef52d85332632c93a0f6628115a67379
fmway <fm18lv@gmail.com>
2026-05-11 13:38:56 +0700

    add clan cache

---
+++ modules/nix/_cache/clan.nix
@@ -0,0 +1,10 @@
+{
+  nix.settings = {
+    substituters = [
+      "https://cache.clan.lol"
+    ];
+    trusted-public-keys = [
+      "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
+    ];
+  };
+}
