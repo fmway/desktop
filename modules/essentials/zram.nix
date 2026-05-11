40c9c4a46a74031093f6e1d293549a99c4e66a75
fmway <fm18lv@gmail.com>
2026-05-11 16:43:33 +0700

    Typos

--- modules/essentials/zram.nix
+++ modules/essentials/zram.nix
@@ -9,7 +9,7 @@
       settings = {
         zram0 = {
           compression-algorithm = "zstd lz4 (type=huge)";
-          ram-size = host.aspect.meta.zram.size or host.aspect.meta.zram-size or "ram / 2";
+          zram-size = host.aspect.meta.zram.size or host.aspect.meta.zram-size or "ram / 2";
           fs-type = "swap";
           swap-priority = host.aspect.meta.zram.priority or host.aspect.meta.swap-priority or 100;
         };
