30cfa1659c361e7bdab57f0ceb79c9699874ef87
fmway <fm18lv@gmail.com>
2026-05-26 12:49:54 +0700

    Add param helium browsers (prefer wayland)

--- modules/browsers/helium.nix
+++ modules/browsers/helium.nix
@@ -2,10 +2,21 @@
   fmx.browsers._.helium.homeManager =
     { inputs', ... }:
     {
-      home.packages = [ inputs'.helium.packages.default ];
+      home.packages = [
+        (inputs'.helium.packages.default.overrideAttrs (o: {
+          installPhase = builtins.replaceStrings 
+            [ "makeWrapper $out/opt/helium/helium $out/bin/helium \\" "--enable-features=" ] [ /* sh */ ''
+              makeWrapper $out/opt/helium/helium $out/bin/helium \
+                --add-flags "--ozone-platform=wayland" \
+                --add-flags "--use-gl=angle" \
+                --add-flags "--use-angle=gl" \''
+              "--enable-features=VaapiVideoDecodeLinuxGL,TouchpadOverscrollHistoryNavigation,AcceleratedVideoEncoder,VaapiVideoEncoder,Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport,UseMultiPlaneFormatForHardwareVideo,"
+            ] o.installPhase;
+        }))
+      ];
     };
-  fmx.browsers._.helium = {
-    includes = [
+    fmx.browsers._.helium = {
+      includes = [
       ({ host, user, persistent, ... }: {
         persistence.${persistent.cacheDirectory}.users.${user.userName}.directories = [
           ".cache/net.imput.helium"
