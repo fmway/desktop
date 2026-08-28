3f376bc3b8f59c3de5bf0b214534603dbed47dd9
fmway <fm18lv@gmail.com>
2026-08-28 18:20:39 +0700

    nixpak: Telegram

--- modules/desktops/apps/telegram.nix
+++ modules/desktops/apps/telegram.nix
@@ -3,10 +3,40 @@
     includes = [
       ({ persistent, user, ... }: {
         persistence.${persistent.cacheDirectory}.users.${user.userName}.directories = [
-          ".local/share/TelegramDesktop"
+          ".var/app/org.telegram.desktop"
         ];
       })
     ];
+
+    # ref: https://github.com/mnixry/nixos-config/blob/main/pkgs/nixpaks/telegram.nix
+    nixpak.telegram-desktop = { pkgs, sloth, ... }:
+    {
+      app.package = pkgs.telegram-desktop;
+      flatpak.appId = "org.telegram.desktop";
+
+      # FIXME: or maybe we use another sandbox tools?
+      # bubblewrap.bind = with sloth; {
+      #   ro = [
+      #     (concat' homeDir "/Downloads")
+      #   ];
+      #   rw = [
+      #     (concat' homeDir "/Downloads/Telegram Desktop")
+      #   ];
+      # };
+
+      dbus = {
+        enable = true;
+        policies = {
+          "org.gnome.Mutter.IdleMonitor" = "talk";
+          "org.freedesktop.Notifications" = "talk";
+          "org.kde.StatusNotifierWatcher" = "talk";
+          "com.canonical.AppMenu.Registrar" = "talk";
+          "com.canonical.indicator.application" = "talk";
+          "org.ayatana.indicator.application" = "talk";
+          "org.sigxcpu.Feedback" = "talk";
+        };
+      };
+    };
     homeManager = { pkgs, ... }:
     {
       home.packages = [ pkgs.telegram-desktop ];
