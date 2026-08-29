9a5e95a65310f36c69fb18ee53c00fdec78e6f42
fmway <fm18lv@gmail.com>
2026-08-29 19:02:47 +0700

    Add some jail combinators (env, regex, & loose)

--- modules/tools/sandbox/jail/lib.nix
+++ modules/tools/sandbox/jail/lib.nix
@@ -1,7 +1,7 @@
 { inputs, require, lib, ... }: let
 
   combinators = c: let
-    normalizePath = p: if isStartWithTilde p then c.noescape p else p;
+    normalizePath = p: if !p?_noescape && isStartWithTilde p then c.noescape p else p;
   in {
     rw'= p: c.try-readwrite (normalizePath p);
     ro = p: c.try-readonly (normalizePath p);
@@ -9,20 +9,84 @@
       (c.add-runtime /* sh */ "[ -d ${p_} ] || mkdir ${p_}")
       (c.readwrite p')
     ];
-    env = c.set-env;
+    regex = _regex: { inherit _regex; };
+
+    # (env (regex "A_REGEX")) -> import all env that the name contains A_REGEX
+    # (env "A_VAR" "a_value") -> set A_VAR to a_value
+    # (env "A_VAR") -> import A_VAR
+    env = name:
+      if name ? _regex && builtins.isString name._regex then
+        c.compose [
+          (state: state // {
+            envs = state.envs or [] ++ [ name._regex ];
+          })
+          (c.include-once "collect-envs" (c.defer collect-envs))
+        ]
+      else x: (if builtins.isString x then c.set-env else c.try-fwd-env) name x;
+
+    # importing PATH, useful if combine with dev environment
+    loose = c.compose [
+      (c.rw "/nix/store")
+      (c.ro "/etc/nix")
+      (c.ro "/run/current-system")
+      (c.ro (c.noescape "\"/etc/profiles/per-user/$USER\""))
+      (c.add-path "\"$PATH\"")
+    ];
+  };
+
+  parseRegex = str: let
+    len = builtins.stringLength str;
+    hasPrefix = builtins.substring 0 1 str == "^";
+    hasSuffix =
+      builtins.substring (len - 1) 1 str == "$" &&
+      builtins.substring (len - 2) 1 str != "\\";
+    h = if hasPrefix then 1 else 0;
+    t = len - (if hasSuffix then 1 else 0) - h;
+    start = if hasSuffix || !hasPrefix then "[^= ]*" else "";
+    end   = if hasPrefix || !hasSuffix then "[^= ]*" else "";
+    final = builtins.substring h t str;
+  in if hasPrefix && hasSuffix then final else "${start}${final}${end}";
+
+  collect-envs = state: let
+    regex = "^(${lib.concatMapStringsSep "|" parseRegex (state.envs or [])})=.*$";
+  in state // {
+    runtime = state.runtime + "\n" + /* sh */ ''
+      mapfile -t -d "" -O "''${#RUNTIME_ARGS[@]}" RUNTIME_ARGS < <(printenv --null | grep --null-data -E "${regex}" | while IFS= read -r -d "" e; do
+        printf -- '--setenv\0%s\0%s\0' "''${e%%=*}" "''${e#*=}"
+      done)
+    '';
   };
 
   isStartWithTilde = s: builtins.substring 0 1 s == "~";
 
-  jailNix = import "${inputs.jail-nix}/lib/jail.nix";
-  mkJail = { combinators ? _: {}, }: let
+  patchJail = builtins.toFile "jail.nix" (builtins.replaceStrings [
+    "./"
+    "additionalCombinators ? _: { }"
+    "allCombinators = builtinCombinators // additionalCombinators builtinCombinators"
+  ] [
+    "${inputs.jail-nix}/lib/"
+    "overlays ? []"
+    "allCombinators = lib.fix (lib.extends (lib.composeManyExtensions overlays) (_: builtinCombinators))"
+  ] (builtins.readFile "${inputs.jail-nix}/lib/jail.nix"));
+  jailNix = import patchJail;
+  mkJail = { overlays ? [], }: let
     self = {
-      init = pkgs: let s = jailNix { inherit pkgs; suppressExperimentalWarnings = true; additionalCombinators = _: combinators s; }; in s;
+      init = pkgs: jailNix {
+        inherit pkgs overlays;
+        suppressExperimentalWarnings = true;
+      };
       parse = pkgs: jails: let
         jail = self.init pkgs;
         inherit (jail) combinators;
       in builtins.zipAttrsWith (appName: vs: let
-        r = builtins.zipAttrsWith (k: v: if k == "permissions" then builtins.concatMap (fn: fn combinators) v else lib.fmway.resolvePriority v) vs;
+        r = builtins.zipAttrsWith (k: v:
+          if k == "permissions" then
+            builtins.concatMap (x: {
+              lambda = x combinators;
+              list = x;
+              null = [];
+            }.${builtins.typeOf x} or (throw "unknown type of permissions, allowed: null, function, list")) v
+          else lib.fmway.resolvePriority v) vs;
         pkg = jail appName r.package r.permissions // { wrapped = pkg; unwrapped = r.package; };
       in pkg) jails;
       # mkOverlays = builtins.zipAttrsWith (appName: vs: _: pkgs: let
@@ -31,10 +95,9 @@
       #   pkg = jail appName r.package r.permissions // { wrapped = pkg; unwrapped = r.package; };
       # in pkg);
       addCombinators = fn: mkJail {
-        combinators = jail: let
-          r = combinators jail // fn c;
-          c = jail.combinators // r;
-        in r;
+        overlays = overlays ++ [
+          (self: let r = fn self; in if builtins.isFunction r then r else _: r)
+        ];
       };
     };
   in self;
